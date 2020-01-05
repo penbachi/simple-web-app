44c1e1edfccdbd5a75eb9260412a32d582250c35
Bhaskar <36286534+penbachi@users.noreply.github.com>
2020-01-05 16:18:41 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import flask
+app = flask(_name_)
+
+@app.route("/");
+def main():
+	retrun "Welcome!"
+
+@app.route("/how are you");
+def hello
+	return "I am good, how about you?"
+
+if _name_ == "_main_":
+	app.run()
